446d5ae6a0d9b1571fc89bffa2cbac3b1e302961
Amr Hamouda <amr.hamouda@gmail.com>
2025-07-29 21:41:15 +0300

    update compileSdk and ndkVersion in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.example.battery_alarm"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
          isCoreLibraryDesugaringEnabled = true
