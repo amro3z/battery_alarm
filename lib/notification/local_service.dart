b7cc4c99578aeb4b00bbfc40c7c5f14de4deac63
Amr Hamouda <amr.hamouda@gmail.com>
2025-07-29 23:30:16 +0300

    add function to show basic notification

--- lib/notification/local_service.dart
+++ lib/notification/local_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class LocalService {
@@ -5,7 +6,40 @@ class LocalService {
       FlutterLocalNotificationsPlugin();
 
   initNotification() async {
-    InitializationSettings settings = InitializationSettings();
-    await flutterLocalNotification.initialize(settings);
+    InitializationSettings settings = InitializationSettings(
+      android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+      iOS: DarwinInitializationSettings(),
+    );
+    await flutterLocalNotification.initialize(
+      settings,
+      onDidReceiveNotificationResponse: (NotificationResponse response) {
+        // Handle notification response
+        debugPrint("User clicked notification → payload: ${response.payload}");
+      },
+      onDidReceiveBackgroundNotificationResponse:
+          (NotificationResponse response) {
+            // Handle background notification response
+            debugPrint(
+              "Background notification clicked → payload: ${response.payload}",
+            );
+          },
+    );
+  }
+
+  showBasicNotification() {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      channelDescription: 'your_channel_description',
+      importance: Importance.max,
+      priority: Priority.high,
+      showWhen: false,
+    );
+    var iOSPlatformChannelSpecifics = DarwinNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics,
+    );
+    flutterLocalNotification.show(0, "title", "body", platformChannelSpecifics);
   }
 }
